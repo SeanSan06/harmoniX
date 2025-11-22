8175c33eeb5af45589942c73c31466a7414c1a52
Sean San <seanksan@gmail.com>
2025-11-22 01:26:53 -0800

    Refactor code for HTML, CSS, and JS
    Reorganize how comments are written so they are more clear. Spaced out
    the HMTL and CSS code to look cleaner. Add comments to the JS functions
    that explain what each is doing.

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -1,10 +1,11 @@
+// A sleep function that takes in 'ms' as input and pauses for that amount of time
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+// When page is loaded or reloaded run the slide to left animation and make image fade in
 window.addEventListener("load", async () => {
     await sleep(1600);
-
     document.querySelector("#harmoniXfer-title").classList.add("animate");
     document.querySelector("#harmoniXfer-caption").classList.add("animate");
     document.querySelector("#transfer-song-button").classList.add("animate");
