71114bfbb8c5ce493f26776d4e96a39db02a3bf4
Sean San <seanksan@gmail.com>
2025-11-27 00:28:37 -0800

    Fix TypeScript not integrating with website
    The TypeScript file needs to compile into a main.js. However, this led
    to some issues with pathing and the tsconfig.json. This commit fixes
    these issues and allows the main.ts to be compiled properly from now
    on.

---
+++ frontend/javascript/main.js
@@ -0,0 +1,24 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+window.addEventListener("load", () => __awaiter(void 0, void 0, void 0, function* () {
+    console.log("Testing testing");
+    yield sleep(1600);
+    document.querySelector("#harmoniXfer-title").classList.add("animate");
+    document.querySelector("#harmoniXfer-caption").classList.add("animate");
+    document.querySelector("#transfer-song-button").classList.add("animate");
+    document.querySelector("#title-caption-button").classList.add("animate");
+    yield sleep(900);
+    document.querySelector("#image").classList.add("appear-fade-in");
+}));
+//# sourceMappingURL=main.js.map
---
+++ frontend/ts/main.ts
@@ -0,0 +1,16 @@
+
+function sleep(ms: number): Promise<void> {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+window.addEventListener("load", async () => {
+    console.log("Testing testing");
+    await sleep(1600);
+    document.querySelector("#harmoniXfer-title")!.classList.add("animate");
+    document.querySelector("#harmoniXfer-caption")!.classList.add("animate");
+    document.querySelector("#transfer-song-button")!.classList.add("animate");
+    document.querySelector("#title-caption-button")!.classList.add("animate");
+
+    await sleep(900);
+    document.querySelector("#image")!.classList.add("appear-fade-in");
+});
